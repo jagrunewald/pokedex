4a82ae96c4eae8624afae52b017ea064a885b74e
jagrunewald <je_grunewald@hotmail.com>
2022-09-13 19:07:06 -0300

    :recycle: refactor: Refactored Pokemons component: Changed request to bring all pokemons on home screen and to search specific pokemon in search input

--- src/Components/Pokemons/Pokemons.js
+++ src/Components/Pokemons/Pokemons.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import getPokemon from "../../services/getPokemon";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CardPokemon from '../CardPokemon/CardPokemon';
 import imgSearch from '../../img/search.png';
 import './Pokemons.css';
 
 export default function Pokemons () {
-  const [search, setSearch] = useState('');
+  const [pokemons, setPokemons] = useState([]);
+  
+  const searchPokemon = (name) => {
+    var filterPokemons = [];
+    if (name === '') {
+      fetchPokemons();
+    }
 
-  function handleChange({ target }) {
-    setSearch(target.value);
+    for (var i in pokemons) {
+      if(pokemons[i].data.name.includes(name)) {
+        filterPokemons.push(pokemons[i])
+      }
+    }
+    setPokemons(filterPokemons);
   }
 
-  async function handleClick () {
-    const results = await getPokemon(search);
-    console.log('results',results);
-    return results;
-  };
+  const fetchPokemons = () => {
+    var endpoints = [];
+    for (var i = 1; i < 49; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+    var result = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+    return result;
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  },[]);
 
   return (
     <div className='pokemons'>
-      <div className='pokemons-search'>
-        <img src={imgSearch} alt='Search...'/>
-        <input
-          type='text'
-          className='input-search'
-          placeholder='Search...'
-          value={search}
-          onChange={ handleChange }
-        />
-        <button type='button' className='btn-search' onClick={ handleClick }>Search</button>
+      <div className='pokemons-menu'>
+        <div className='pokemons-search'>
+          <img src={imgSearch} alt='Search...'/>
+          <input
+            type='text'
+            className='input-search'
+            placeholder='Search...'
+            onChange={(e) => searchPokemon(e.target.value)}
+          />
+        </div>
       </div>
-      <div>
-        <h1>teste</h1>
+      <div className='pokemons-all'>
+        {
+          pokemons.map((pokemon, index) => {
+            return (
+              <CardPokemon key={index} pokemon={pokemon} />
+            )
+          })
+        }
       </div>
     </div>
   )
--- src/services/getPokemon.js
+++
@@ -1,8 +0,0 @@
-const getPokemon = async (name) => {
-  const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const requestJson = await request.json();
-  // const pokemons = requestJson.sprites.front_default;
-  return requestJson;
-};
-
-export default getPokemon;
